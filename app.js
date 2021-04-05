e5aa1b9acf74306c1f8d5d672a996e95276c2c86
Didina Vicliuc <didinacravcenco802@gmail.com>
2021-04-05 18:59:02 +0300

    hide OPEN WeatherMAp API key

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 //const argv = process.argv;
+import { OPEN_WEATHER_MAP_API_KEY } from "./credential.js";
 const city = process.argv[2];
 async function printCurrentWeather(cityName) {
   const OPEN_WEATHER_MAP_API =
-    "http://api.openweathermap.org/data/2.5/weather?q=" +
-    cityName +
-    "&appid=***REMOVED***&units=metric&lang=ro";
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}` +
+    `&appid=${OPEN_WEATHER_MAP_API_KEY}&units=metric&lang=ro`;
   try {
     const response = await axios.get(OPEN_WEATHER_MAP_API);
     let data = response.data;
